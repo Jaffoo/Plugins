7316bb2929d9715325c12fa71e48443d7ce2c670
Jaffoo <gaffoo@163.com>
2024-04-01 09:05:15 +0800

    添加项目文件。

---
+++ Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Xml;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = File.Exists("E:\\git\\Plugins\\ChatGPT\\bin\\Release\\net8.0\\publish\\ChatGPT.dll");
+            var dll = Assembly.LoadFrom("E:\\git\\Plugins\\ChatGPT\\bin\\Release\\net8.0\\publish\\ChatGPT.dll");
+            Type[] types = dll.GetTypes();
+
+            foreach (Type t in types)
+            {
+                Console.WriteLine(t.FullName);
+            }
+        }
+    }
+}
