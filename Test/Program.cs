94d73f2e79c1e8028f48a04f7890d0fb482b227c
Jaffoo <gaffoo@163.com>
2024-09-24 10:01:58 +0800

    1

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,18 +10,7 @@ namespace Test
     {
         static async Task Main()
         {
-            var a =await new SpeakWell().Abbreviations("yyds");
-            var timeDifference = DateTime.Now.ToTimeStamp(false);
-            await new DouYu().CheckLiveV2("5377721");
-            Bot bot = new(new("www.zink.asia", 3001, 3000));
-            await bot.StartAsync();
-            bot.MessageReceived.OfType<PrivateReceiver>().Subscribe(async x =>
-            {
-                await new ChatGPT().FriendMessage(x);
-                await new SixtySeeWorld().FriendMessage(x);
-                await new Oil().FriendMessage(x);
-                await new DouYu().FriendMessage(x);
-            });
+            
             while (true)
             {
                 Thread.Sleep(10);
