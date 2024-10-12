f5d41f471fa43f274a02b3aab6a9b8fc6d78c6a0
Jaffoo <gaffoo@163.com>
2024-10-12 12:03:46 +0800

    1

--- ZDownloadDLL/Program.cs
+++ ZDownloadDLL/Program.cs
@@ -2,56 +2,26 @@
 using Newtonsoft.Json.Linq;
 using System.Net;
 
-namespace UmoBot.DownloadDLL;
+namespace DownloadDLL;
 
 internal class Program
 {
     static async Task Main()
     {
-        var pkgName = await GetLastVersion();
+        var pkgName = await GetPkgLastVersion();
         if (string.IsNullOrWhiteSpace(pkgName))
         {
             Console.WriteLine("未发现到包");
             Console.ReadKey();
             return;
         }
-        var clientHandler = new HttpClientHandler
-        {
-            AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
-        };
-        var client = new HttpClient(clientHandler);
-        var request = new HttpRequestMessage
-        {
-            Method = HttpMethod.Get,
-            RequestUri = new Uri("https://raw.githubusercontent.com/Jaffoo/QQBotPlugin/refs/heads/master/IPluginBase/bin/Debug/" + pkgName),
-            Headers =
-            {
-                { "accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7" },
-                { "accept-language", "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6" },
-                { "cache-control", "no-cache" },
-                { "pragma", "no-cache" },
-                { "priority", "u=0, i" },
-                { "sec-ch-ua", "\"Microsoft Edge\";v=\"129\", \"Not=A?Brand\";v=\"8\", \"Chromium\";v=\"129\"" },
-                { "sec-ch-ua-mobile", "?0" },
-                { "sec-ch-ua-platform", "Windows" },
-                { "sec-fetch-dest", "document" },
-                { "sec-fetch-mode", "navigate" },
-                { "sec-fetch-site", "none" },
-                { "sec-fetch-user", "?1" },
-                { "upgrade-insecure-requests", "1" },
-                { "user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0" },
-                { "Accept", "*/*" },
-                { "User-Agent", "PostmanRuntime-ApipostRuntime/1.1.0" },
-                { "Connection", "keep-alive" },
-            },
-        };
-        using var response = await client.SendAsync(request);
-        response.EnsureSuccessStatusCode();
-        var stream = await response.Content.ReadAsStreamAsync();
         var cPath = Directory.GetCurrentDirectory();
         var pPath = Directory.GetParent(cPath)!.FullName;
         var path = Path.Combine(pPath, "BaseUsing", "nuget");
         if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+        foreach (var file in Directory.GetFiles(path))
+            File.Delete(file);
+        using var stream = await GetPkgStream(pkgName);
         var filePath = Path.Combine(path, pkgName);
         using FileStream fileStream = new(filePath, FileMode.Create, FileAccess.Write, FileShare.None);
         await stream.CopyToAsync(fileStream);
@@ -59,8 +29,7 @@ internal class Program
         Console.WriteLine("按任意键退出");
         Console.ReadKey();
     }
-
-    private static async Task<string> GetLastVersion()
+    private static async Task<string> GetPkgLastVersion()
     {
         var clientHandler = new HttpClientHandler
         {
@@ -105,5 +74,42 @@ internal class Program
             .OrderByDescending(name => name) // 按字典序降序排序
             .FirstOrDefault();
         return latestKey ?? "";
+    }
+    private static async Task<Stream> GetPkgStream(string pkgName)
+    {
+        var clientHandler = new HttpClientHandler
+        {
+            AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
+        };
+        var client = new HttpClient(clientHandler);
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri("https://raw.githubusercontent.com/Jaffoo/QQBotPlugin/refs/heads/master/IPluginBase/bin/Debug/" + pkgName),
+            Headers =
+            {
+                { "accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7" },
+                { "accept-language", "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6" },
+                { "cache-control", "no-cache" },
+                { "pragma", "no-cache" },
+                { "priority", "u=0, i" },
+                { "sec-ch-ua", "\"Microsoft Edge\";v=\"129\", \"Not=A?Brand\";v=\"8\", \"Chromium\";v=\"129\"" },
+                { "sec-ch-ua-mobile", "?0" },
+                { "sec-ch-ua-platform", "Windows" },
+                { "sec-fetch-dest", "document" },
+                { "sec-fetch-mode", "navigate" },
+                { "sec-fetch-site", "none" },
+                { "sec-fetch-user", "?1" },
+                { "upgrade-insecure-requests", "1" },
+                { "user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0" },
+                { "Accept", "*/*" },
+                { "User-Agent", "PostmanRuntime-ApipostRuntime/1.1.0" },
+                { "Connection", "keep-alive" },
+            },
+        };
+        var response = await client.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+        var stream = await response.Content.ReadAsStreamAsync();
+        return stream;
     }
 }
